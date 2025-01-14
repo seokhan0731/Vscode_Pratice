c3c17a6a2c4409f4fa1e2e8f95762f58ec44534b
seokhan0731 <wistin@naver.com>
2025-01-14 20:08:24 +0900

    last 7568

--- 7568_덩치.c
+++ 7568_덩치.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 typedef struct big{
     int weight;
     int height;
@@ -8,23 +9,31 @@ typedef struct big{
 void get_rank(BIG * array,int n, int rank[]);
 
 int main(){
+
 int n;
 scanf("%d",&n);
+
 BIG * array=(BIG*)malloc(n*sizeof(BIG));
+
 for(int i=0;i<n;i++){
     scanf("%d %d",&array[i].height,&array[i].weight);
 }
+
 int *rank=(int*)malloc(n*sizeof(int));
+
 get_rank(array,n,rank);
+
 for(int i=0;i<n;i++){
     printf("%d ",rank[i]);
 }
+
 free(array);
 free(rank);
+
 return 0;
 }
 
-
+//문제 조건에 맞게 랭킹 구하기
 void get_rank(BIG * array,int n, int rank[]){
 int count=0;
 for(int i=0;i<n;i++){
@@ -37,5 +46,4 @@ for(int i=0;i<n;i++){
     }
     rank[i]=count+1;
 }
-
 }
